ee477cbfea602cf15d7db5d334515b327d05ec76
Victor Eduardo Barreto <victor.eduardo.barreto@gmail.com>
2016-10-05 00:42:39 -0300

    [add] new files

---
+++ CodeTerminal.py
@@ -0,0 +1,46 @@
+import sublime
+import sublime_plugin
+import subprocess
+import os
+
+class CodeTerminalCommand(sublime_plugin.WindowCommand):
+
+    previous = ""
+
+    print("............::::::::| CodeTerminal | Started |::::::::............")
+
+    def run(self):
+        sublime.active_window().show_input_panel('Command', self.previous, self.cmd, 'None', 'None')
+
+    def cmd(self, query):
+
+        self.previous = query
+
+        panel = sublime.active_window().create_output_panel("xxx")
+        sublime.active_window().run_command("show_panel", {"panel": "output." + "xxx"})
+        
+        sublime.status_message('CodeTermial | Running command')
+
+        try:
+            os.chdir(sublime.active_window().extract_variables()['folder'])
+
+        except Exception as e:
+            pass
+            
+        results, errors = subprocess.Popen(query, stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=True).communicate()
+
+        panel.set_read_only(False)
+        panel.run_command('append', {'characters': "[Output]\n"})
+        panel.run_command('append', {'characters': "=====\n"})
+        panel.run_command('append', {'characters': "\n"})
+        
+        if errors:
+            panel.run_command('append', {'characters': "[Error]\n"})
+            result = errors.decode('utf-8', 'replace').replace('b\"\'"', '')
+
+        else:
+            result = results.decode('utf-8', 'replace').replace('b\"\'"', '')
+
+        panel.run_command('append', {'characters': result})
+        panel.run_command('append', {'characters': "\n[Finished]"})
+        panel.set_read_only(True)
